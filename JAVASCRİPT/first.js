4ca622bfa40b5ea5d2c7dbf57dd95af101efe199
zeyra19 <you@example.com>
2024-01-17 22:08:50 +0300

    new files

---
+++ JAVASCRİPT/first.js
@@ -0,0 +1,21 @@
+//document write ile çıktı veririz pek kullanılmaz
+//document.writeln("Hello World") 
+//altına bir şey daha yazdığında line yani satır bırakır.
+//bi tane daha aşağı inmesini istersen <br> kullanabilirsin
+//document.write("Merhaba Dünya")
+
+//console log metodunu kullanırız
+console.log("Hello World");
+console.log(true)
+console.log({name:"zehra", surname:"cart curt"})
+
+//konsolu temizlemek için console.clear();
+
+let a=5;
+let b=10;
+console.log(a+b); //ikisinin toplamını konsolo yazdırmak için yaptım
+//console.log("iki sayının toplamı:" ,a+b); şeklinde de yapabiliirm
+
+//pop up kullanmak için= alert("pop up mesajı");
+
+
