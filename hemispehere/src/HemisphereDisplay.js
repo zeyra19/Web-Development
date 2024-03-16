c5f2584b52fa608b3c037bcbf84c35f89d8115ba
zeyra19 <zehranurhindioglu@gmail.com>
2024-03-16 08:02:58 +0300

    react for everyone

--- hemispehere/src/HemisphereDisplay.js
+++ hemispehere/src/HemisphereDisplay.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import './Hemisphere.css';
+import northernPic from './images/northen.jpg';
+import southernPic from './images/sorthen.jpg';
 
-const HemisphereDisplay = (props) => {
-    return(
-        <div>
-            Şu an kuzey yarım küredesin
+const hemisphereConfig = {
+    Northern: {
+        text: "burası kuzey yarım küre",
+        picture: northernPic
+    },
+    Southern: {
+        text: "burası güney yarım küre",
+        picture: southernPic
+    }
+}
+const HemisphereDisplay = ({ latitude }) => {
+
+    const hemisphere = latitude > 0 ? 'Northern': 'Southern'; //İFLİ YAPI
+    const { text, picture } = hemisphereConfig[hemisphere]
+
+    return (
+        <div className={hemisphere}>
+            <div className='ui raised text container segment'>
+                <div className='image'>
+                    <img src={picture} alt='hemisphere picture'></img>
+                </div>
+                <div className='ui teal bottom attached label'> 
+                {/* css özelliğini kullandık texte */}
+                    <h1> {text} </h1>
+                </div>
+            </div>
         </div>
     )
 }
