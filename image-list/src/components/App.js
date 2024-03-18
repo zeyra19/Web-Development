094c4c0bf214bd1f3d0feb1451699a4c3d103781
zeyra19 <zehranurhindioglu@gmail.com>
2024-03-18 07:47:41 +0300

    react for everyone

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 import SearchInput from './Searchinput';
+import ImageList from './imageList';
+
 
 class App extends React.Component {
 
+    state = { images: [] }
 
-    onSearchSubmit = (entry) => {
+    onSearchSubmit = async (entry) => {
+        const response = await axios.get(`https://pixabay.com/api/?key=42931583-1f616c5277f6d77186c460c5f&q=${entry} &image_type=photo`)
+        // kullanıcının girdiği veriyi almak istiyorum onsearchsubmite girdiğim entry ile değiştirdim
+        // verinin gelmesi zaman alacağı için async kullandım
 
+        this.setState({ images: response.data.hits })
     }
 
 
@@ -13,6 +21,8 @@ class App extends React.Component {
         return (
             <div className='ui container' style={{ marginTop: '30px' }}>
                 <SearchInput onSearchSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
+                {/* images adında props ekledim */}
             </div>
         )
     }
