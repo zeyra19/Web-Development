6d3234e4785eabb7fdb6ebfbb7bdf72671221f3a
zeyra19 <zehranurhindioglu@gmail.com>
2024-03-19 21:23:26 +0300

    react for everyone

--- yonlendirici/src/App.js
+++ yonlendirici/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'; // navbarı tüm sayfalarda göstermek istediğim için App.js de tanımlıyorum
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar'; // navbarı tüm sayfalarda göstermek istiyorum
 import Home from './components/Home';
 import Contact from './components/Contact';
 import About from './components/About';
@@ -9,12 +9,12 @@ import About from './components/About';
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Route path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
-      </div>
+      <Navbar />
+      <Routes >
+        <Route path='/' element={< Home /> } />
+        <Route path='/about' element={ <About />} />
+        <Route path='/contact' element={ <Contact />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- yonlendirici/src/components/About.js
+++ yonlendirici/src/components/About.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Card, CardContent, Grid } from '@mui/material';
 
 const About = () => {
+    const [label, setLabel] = useState("zart")
+    const handleClick = () => {
+        setLabel("zort gldi")
+    }
     return (
-        <div
-            className='ui raised very padded text container segment '
-            style={{marginTop: '80px'}}>
 
-        <h3 className='ui header'>Hakkımızda</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>  
-        </div>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {Array.from(Array(6)).map((_, index) => (
+                <Grid item xs={2} sm={4} md={4} key={index}>
+                    <Card >
+                        <CardContent>xs=2</CardContent>
+                    </Card>
+                </Grid>
+            ))}
+            <Grid item xs={8}><Card >
+                <CardContent>{label}  </CardContent>
+            </Card>
+            </Grid>
+            <Grid item xs={4}><Card >
+                <CardContent>
+                    <Button onClick={ handleClick }>ZORT</Button>
+                    </CardContent>
+
+            </Card>
+            </Grid>
+
+            
+        </Grid>
+
     )
 }
 
